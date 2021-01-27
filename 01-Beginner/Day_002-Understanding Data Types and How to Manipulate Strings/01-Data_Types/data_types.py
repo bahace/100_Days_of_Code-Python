0f49e060f7881846380c21dcfda260e6dd4ce65c
Joey Davis <joeyrendar@gmail.com>
2021-01-27 16:48:46 -0600

    Day 2  updates

---
+++ 01-Beginner/Day_002-Understanding Data Types and How to Manipulate Strings/01-Data_Types/data_types.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+digit_0 = int(two_digit_number[0])
+digit_1 = int(two_digit_number[1])
+
+message = digit_0 + digit_1
+print(message)
