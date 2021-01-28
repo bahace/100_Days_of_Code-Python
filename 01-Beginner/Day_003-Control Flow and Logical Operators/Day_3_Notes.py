74cb379009aef403fd9c636194451878f5a76aac
joey davis <joeyrendar@gmail.com>
2021-01-28 15:12:58 -0600

    day 3 start

---
+++ 01-Beginner/Day_003-Control Flow and Logical Operators/Day_3_Notes.py
@@ -0,0 +1,21 @@
+# Notes for 3.27
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+# if we want to include 120, use ">="
+if height > 120:
+    print("Let's Ride!!!!")
+else:
+    print("you need to grow a little more :(")
+
+# comparison operators
+# > Greater than
+# < Less than
+# >= Greater than or equal to
+# <= Less than or equal to
+# == Equal to
+# != Not equal to
+
+# modulo
+print(7 % 2)
+# modulo gives you the remainder
