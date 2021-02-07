869334aa336f899fa33b77a3d8012a9b1528b302
joey davis <joeyrendar@gmail.com>
2021-02-06 18:17:58 -0600

    finished day 4.

---
+++ 01-Beginner/Day_004-Randomisation and Python Lists/04-Rock_Paper_Scissors/main.py
@@ -0,0 +1,69 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+import random
+
+# get what the human will throw, convert to int
+human_throw = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors "))
+
+# make a list of the options and print the ascii for the human throw
+rps = [rock,paper,scissors]
+print("You chose")
+print(rps[human_throw])
+
+# formatting print
+print("Computer chose")
+
+# get the cpu throw via random and print the ascii for it
+cpu_throw = random.randint(0,2)
+print(rps[cpu_throw])
+
+# determine and print who wins if human chooses 0/rock
+if human_throw == 0:
+    if cpu_throw == 0:
+        print("Draw")
+    elif cpu_throw == 1:
+        print ("You lose")
+    elif cpu_throw == 2:
+        print ("You win")
+
+# determine and print who wins if human chooses 1/paper
+if human_throw == 1:
+    if cpu_throw == 0:
+        print("You win")
+    elif cpu_throw == 1:
+        print("Draw")
+    elif cpu_throw == 2:
+        print("You lose")
+
+# determine and print who wins if human chooses 2/scissors
+if human_throw == 2:
+    if cpu_throw == 0:
+        print("You lose")
+    elif cpu_throw == 1:
+        print("You win")
+    elif cpu_throw == 2:
+        print("Draw")
--- 01-Beginner/Day_004-Randomisation and Python Lists/Day_4_Notes.py
+++ 01-Beginner/Day_004-Randomisation and Python Lists/Day_4_Notes.py
@@ -12,20 +12,30 @@
 # random_float1 *= 5
 # print(random_float1)
 
-# 4.41 Understanding the offset and appending items to lists
-# lists URL https://docs.python.org/3/tutorial/datastructures.html
-fruits = ["Chery", "Apple", "Pear"]
-us_states = ["Texas", "New York", "Florida"]
-# print the X item in the list
-print(us_states[2])
-# Can also count negative, starts from end of list
-print(us_states[-2])
-# Alter an item in the list
-us_states[2] = "Oklahoma"
-print(us_states[2])
-# add an item at the end of a list
-us_states.append("Davisland")
-print(us_states)
-# extend list - add more then 1 item to it
-us_states.extend(["Brattland", "Redland"])
-print(us_states)
+# # 4.41 Understanding the offset and appending items to lists
+# # lists URL https://docs.python.org/3/tutorial/datastructures.html
+# fruits = ["Chery", "Apple", "Pear"]
+# us_states = ["Texas", "New York", "Florida"]
+# # print the X item in the list
+# print(us_states[2])
+# # Can also count negative, starts from end of list
+# print(us_states[-2])
+# # Alter an item in the list
+# us_states[2] = "Oklahoma"
+# print(us_states[2])
+# # add an item at the end of a list
+# us_states.append("Davisland")
+# print(us_states)
+# # extend list - add more then 1 item to it
+# us_states.extend(["Brattland", "Redland"])
+# print(us_states)
+
+# 4.43 IndexErrors and Working with Nested Lists
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears",
+               "Tomatoes", "Celery", "Potatoes"]
+# how can list out within our current list the fruits and vegetables?
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen[1][2])
