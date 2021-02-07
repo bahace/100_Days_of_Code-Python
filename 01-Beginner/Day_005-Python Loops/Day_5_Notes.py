3f3064e387f7bdb2bc2530945c3dc3b6a953238d
joey davis <joeyrendar@gmail.com>
2021-02-07 15:03:51 -0600

    day 5 start

---
+++ 01-Beginner/Day_005-Python Loops/Day_5_Notes.py
@@ -0,0 +1,10 @@
+# 5.48 Using the for loop with Python Lists
+fruits = ["Apple", "Peach", "Pear"]
+for item in fruits:
+    print(item)
+    print(item + " Pie")
+# inside vs outside for loop
+# inside
+    print(fruits)
+# outside
+print(fruits)
