0485de76d6d2a16ce36fae7fdf550c82443a3113
joey davis <joeyrendar@gmail.com>
2021-02-11 20:51:28 -0600

    day ex3 complete

---
+++ 01-Beginner/Day_005-Python Loops/03-Adding_Evens/main.py
@@ -0,0 +1,11 @@
+# Write your code below this row 👇
+
+# set total to 0 to start
+total = 0
+
+# create for loop in range with an "2" step to capture only even numbers
+for number in range(0, 101, 2):
+    total += number
+
+# print total
+print(total)
--- 01-Beginner/Day_005-Python Loops/Day_5_Notes.py
+++ 01-Beginner/Day_005-Python Loops/Day_5_Notes.py
@@ -3,9 +3,24 @@ fruits = ["Apple", "Peach", "Pear"]
 for item in fruits:
     print(item)
     print(item + " Pie")
-# inside vs outside for loop
-# inside
+    # inside vs outside for loop
+    # inside
     print(fruits)
 # outside
 print(fruits)
 # inside for loops, its proper form to name it the singular form of the list. fruit for list of fruits.
+
+# 5.51 for loops and the range() function
+# for number in range(1, 11, 3):
+# print(number)
+for number in range(1, 10):
+    print(number)
+# ranges dont include last number, but do include the first number
+# we can also change the "step" size by adding a 3rd number in the range function. the 3rd number is the step.:
+for number in range(1, 10, 3):
+    print(number)
+
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
